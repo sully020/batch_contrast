69124f06ff80e9b48f1ce606883e650acede17c2
Chris Sullivan <cs590919@wne.edu>
2023-12-05 21:12:37 -0500

    style: adjust header spacing

--- contrast_calculator.py
+++ contrast_calculator.py
@@ -1,8 +1,10 @@
 
-""" Author: Christopher Sullivan
+""" 
+Author: Christopher Sullivan
 Version: 0.0.1
 
-This script loads in image files and calculates their respective contrast ratios by first converting them to grayscale, then calculating contrast values through both luminescence range and standard luminescence deviation.
+This script loads in image files and calculates their respective contrast ratios by first converting them to grayscale, 
+then calculating contrast values through both luminescence range and standard luminescence deviation.
 """
 
 import cv2 as cv
