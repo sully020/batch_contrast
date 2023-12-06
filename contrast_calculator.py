e5723e95edcfd51c363afb45aae774dddc6fd137
Chris Sullivan <cs590919@wne.edu>
2023-12-05 19:13:03 -0500

    feat: ADD contrast_calculator.py

---
+++ contrast_calculator.py
@@ -0,0 +1,11 @@
+
+""" Author: Christopher Sullivan
+Version: 0.0.1
+
+This script loads in image files and calculates their respective contrast ratios by first converting them to grayscale, then calculating contrast values through both luminescence range and standard luminescence deviation.
+"""
+
+import cv2 as cv
+
+def load_image(filename):
+    pass
