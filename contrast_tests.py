0408ea9d1969c61ee768f17d7a756db57b8487ff
Chris Sullivan <cs590919@wne.edu>
2023-12-05 20:22:12 -0500

    feat: ADD gray convert test

--- contrast_tests.py
+++ contrast_tests.py
@@ -5,5 +5,12 @@ def print_image():
     img = cc.load_image("sample.jpg")
     print(img)
 
+
+def print_gray():
+    img = cc.load_image("sample.jpg")
+    img = cc.convert_gray(img)
+    print(img)
+
 print_image()
+print_gray()
 
