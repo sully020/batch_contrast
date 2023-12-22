4b2fabbbf8426c213a61b159753a782ae4a8778c
Chris Sullivan <cs590919@wne.edu>
2023-12-22 13:46:36 -0500

    test: ADD intensity val getter tests

--- contrast_tests.py
+++ contrast_tests.py
@@ -5,12 +5,23 @@ def print_image():
     img = cc.load_image("sample.jpg")
     print(img)
 
-
 def print_gray():
     img = cc.load_image("sample.jpg")
     img = cc.convert_gray(img)
     print(img)
 
+def find_sample_intens():
+    img = cc.load_image("sample.jpg")
+    print(cc.find_intensities(img))
+
+def find_solid_intens():
+    img = cc.load_image("solid.jpg")
+    print(cc.find_intensities(img))
+
+
 print_image()
 print_gray()
+
+find_sample_intens()
+find_solid_intens()
 
