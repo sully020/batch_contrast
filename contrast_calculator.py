10244a708450b9fa58730542bfaecf6f0a72814e
Chris Sullivan <cs590919@wne.edu>
2023-12-22 18:25:24 -0500

    feat: ADD os import, folder iter function

--- contrast_calculator.py
+++ contrast_calculator.py
@@ -9,6 +9,7 @@ then calculating contrast values through both luminescence range and standard lu
 
 import cv2 as cv
 from numpy import std
+from os import listdir
 
 def load_image(filename):
     img = cv.imread(filename)
@@ -23,14 +24,23 @@ def find_intensities(img):
     max = img.max()
     return (min, max)
 
-def calc_by_range():
-    img = load_image('solid.jpg')
+def calc_by_range(img):
     light_vals = find_intensities(img)
     contrast = (light_vals[1] - light_vals[0])
     return contrast
 
-def calc_by_sd():
-    img = load_image('sample.jpg')
+def calc_by_sd(img):
     sd = std(img)
     sd = float("{:.2f}".format(sd))
     return sd
+
+def iter_dir():
+    dir = input('Please enter directory name/path: ')
+    for img in listdir(dir):
+        fname = dir + '/' + img
+        img_dat = load_image(fname)
+        img_dat = convert_gray(img_dat)
+        print(calc_by_range(img_dat))
+        print(calc_by_sd(img_dat))
+
+iter_dir()
