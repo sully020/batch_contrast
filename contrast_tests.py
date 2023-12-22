e2935852f07d6dcf0218af6f7426b244d1bfd0e8
Chris Sullivan <cs590919@wne.edu>
2023-12-22 18:26:54 -0500

    fix, refactor: convert to gray and fix filenames for all tests

--- contrast_tests.py
+++ contrast_tests.py
@@ -1,27 +1,31 @@
 import contrast_calculator as cc
-import cv2 as cv
 
 def print_sample():
-    img = cc.load_image("sample.jpg")
+    img = cc.load_image("images/sample.jpg")
+    img = cc.convert_gray(img)
     print(img)
 
 def print_solid():
-    img = cc.load_image("sample.jpg")
+    img = cc.load_image("images/sample.jpg")
     img = cc.convert_gray(img)
     print(img)
 
 def find_sample_intens():
-    img = cc.load_image("sample.jpg")
+    img = cc.load_image("images/sample.jpg")
+    img = cc.convert_gray(img)
     print(cc.find_intensities(img))
 
 def find_solid_intens():
-    img = cc.load_image("solid.jpg")
+    img = cc.load_image("images/solid.jpg")
+    img = cc.convert_gray(img)
     print(cc.find_intensities(img))
 
 def compare_outputs():
-    sample = cc.load_image("sample.jpg")
+    sample = cc.load_image("images/sample.jpg")
+    sample = cc.convert_gray(sample)
     sam_vals = cc.find_intensities(sample)
-    solid = cc.load_image("solid.jpg")
+    solid = cc.load_image("images/solid.jpg")
+    solid = cc.convert_gray(solid)
     sol_vals = cc.find_intensities(solid)
 
     assert(sam_vals != sol_vals)
