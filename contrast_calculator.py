9e2f2fe8ee7fc17145ddbbd4f804cc4c6d5f6916
Chris Sullivan <cs590919@wne.edu>
2024-03-02 18:43:35 -0500

    fix: normalize range ratio calculation

--- contrast_calculator.py
+++ contrast_calculator.py
@@ -9,8 +9,9 @@ then calculating contrast values through both luminescence range and standard lu
 
 import cv2 as cv
 from numpy import std
-from os import listdir
+from os import listdir, cpu_count
 
+N = cpu_count()
 
 def convert_gray(img):
     img = cv.cvtColor(img, 7, img, 0) 
@@ -26,24 +27,25 @@ def calc_range(img):
     max = img.max()
     if min == 0:
         min += 1
-    return (str(max) + ": " + str(min))
+    max = (max // min)
+    min = (min // min)
+    return (str(max) + ":" + str(min))
 
 def calc_by_sd(img):
     sd = std(img)
     sd = float("{:.2f}".format(sd))
     return sd
 
-def iter_dir():
-    dir = input('Please enter directory name/path: ')
+def iter_dir(dir):
     for img in listdir(dir):
         fname = dir + '/' + img
         img_dat = load_image(fname)
         print("Contrast by Range - " + img + ": " + calc_range(img_dat))
-        print("Contrast by Standard Deviation - " + img + ": " + str(calc_by_sd(img_dat)))
-        print("----------------")
+        print("Contrast by Standard Deviation - " + img + ": " + str(calc_by_sd(img_dat)) + "\n")
 
 def main():
-    iter_dir()
+    dir = input('Please enter directory name/path: ')
+    iter_dir(dir)
 
 if __name__ == "__main__":
     main()
